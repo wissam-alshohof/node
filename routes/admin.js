d085cb15fec3101db383e579bc925ebcba7dce02
wissam-alshohof <wissam.alshohof@gmail.com>
2023-07-17 00:24:07 +0400

    making routeDirectory dependong on require.main / process.mainModule

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const routeDir = require('./../utils/path');
 const path = require('path');
 
 
 
 router.get('/products',(req,res,next) => {
-    res.sendFile(path.join(__dirname,'..','views','add-product.html'))
+    // res.sendFile(path.join(__dirname,'..','views','add-product.html'))
+    res.sendFile(path.join(routeDir,'views','add-product.html'))
 })
 
 router.post('/list',(req,res,next) => {
