e557a711a43446f6a69222b895534e4195739482
wissam-alshohof <wissam.alshohof@gmail.com>
2023-08-05 04:24:58 +0400

    adding partials for EJS *includes

--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,7 @@ const shopRouter = express.Router();
 
 const log = require('./../console');
 
-//TODO using  { use } don't make full match for the route but get DOES
+//? using  { use } don't make full match for the route but get DOES
 shopRouter.get('/', (req, res, next) => {
     log(req.url);
     res.status(401);
